49da276b7a17094f72f230fab2d403deaa6b6a94
°Ivan-Mandujano° <ivanmandujano594@gmail.com>
2023-10-22 21:23:49 -0600

    Added QRCODE custom
    Added to te package and an example of use in app.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { QRCode } from 'react-native-custom-qr-codes';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <QRCode content='https://reactnative.com'/>
     </View>
   );
 }
